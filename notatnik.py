c0acc3fab2acc3f4514cdfc85b5deb6be3ff1ffe
Mikol167 <mpiotro78@wp.pl>
2025-04-09 14:50:03 +0200

    wip: added remove user function

--- notatnik.py
+++ notatnik.py
@@ -1,16 +1,13 @@
 users: list = [
     {'name': 'Julia', 'location': 'Ząbki', 'posts': 10},
+    {'name': 'Klaudia', 'location': 'Warszawa', 'posts': 10},
+    {'name': 'Szymon', 'location': 'Osrtołeka', 'posts': 10},
 ]
 
 print(users)
 
 
-def add_user(users_data: list) -> None:
-    new_name: str = input('podaj imie nowego znajomego: ')
-    new_location: str = input('podaj nazwę lokalizacji: ')
-    new_posts: int = int(input('podaj liczbe postów: '))
-    users.append({'name': new_name, 'location': new_location, 'posts': new_posts})
 
 
-add_user(users)
+remove_user(users)
 print(users)
--- utils/controller.py
+++ utils/controller.py
@@ -7,4 +7,11 @@ def add_user(users_data: list) -> None:
         new_name: str = input('podaj imie nowego znajomego: ')
         new_location: str = input('podaj nazwę lokalizacji: ')
         new_posts: int = int(input('podaj liczbe postów: '))
-        users.append({'name': new_name, 'location': new_location, 'posts': new_posts})
+        users_data.append({'name': new_name, 'location': new_location, 'posts': new_posts})
+
+
+def remove_user(users_data: list) -> None:
+    user_name: str = input('Wpisz kogo chcesz usunąć: ')
+    for user in users_data:
+        if user['name'] == user_name:
+            users_data.remove(user)
